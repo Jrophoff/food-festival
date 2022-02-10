e0fd452d5a6f9b96ba4a3e81e76d1942a527008b
Jordan Ophoff <noreply>
2022-02-09 23:33:45 -0500

    added offline capability

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,14 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    app: './assets/js/script.js',
-    events: './assets/js/events.js',
-    schedule: './assets/js/schedule.js',
-    tickets: './assets/js/tickets.js',
+    app: './public/assets/js/script.js',
+    events: './public/assets/js/events.js',
+    schedule: './public/assets/js/schedule.js',
+    tickets: './public/assets/js/tickets.js',
   },
   output: {
     filename: '[name].bundle.js',
-    path: __dirname + '/dist',
+    path: __dirname + '/public/dist',
   },
   module: {
     rules: [
